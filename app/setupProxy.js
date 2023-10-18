d8e9011ab6f68aa3206c193c3a2d8f462f1f02cb
LiuJingZ <346197701@qq.com>
2023-10-18 16:59:25 +0800

    fix: 跨域

--- app/setupProxy.js
+++ app/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
     // proxy第一个参数为要代理的路由
     // 第二参数中target为代理后的请求网址，changeOrigin是否改变请求头，其他参数请看官网
-    app.use(proxy('/cityjson', {
+    app.use(proxy('https://iframe-wearablepack.vercel.app/', {
         target: 'https://opensea.io/assets/matic/',
         changeOrigin: true
     }))
