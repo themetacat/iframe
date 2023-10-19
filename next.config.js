597cf28e41e6443bf3f182b73682d006079ed6de
LiuJingZ <346197701@qq.com>
2023-10-19 15:19:34 +0800

    fix:

--- next.config.js
+++ next.config.js
@@ -5,24 +5,24 @@
 // module.exports = nextConfig;
 
 
-module.exports = (phase, { defaultConfig }) => {
-    return {
-      ...defaultConfig,
+// module.exports = (phase, { defaultConfig }) => {
+//     return {
+//       ...defaultConfig,
   
-      webpack: (config) => {
-        config.resolve = {
-          ...config.resolve,
-          fallback: {
-            "fs": false,
-            "path": false,
-            "os": false,
-          }
-        }
-        return config
-      },
-    }
+//       webpack: (config) => {
+//         config.resolve = {
+//           ...config.resolve,
+//           fallback: {
+//             "fs": false,
+//             "path": false,
+//             "os": false,
+//           }
+//         }
+//         return config
+//       },
+//     }
     
-  }
+//   }
   module.exports = {
     async headers() {
       return [
@@ -34,6 +34,21 @@ module.exports = (phase, { defaultConfig }) => {
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
+        },
+        {
+          ...defaultConfig,
+      
+          webpack: (config) => {
+            config.resolve = {
+              ...config.resolve,
+              fallback: {
+                "fs": false,
+                "path": false,
+                "os": false,
+              }
+            }
+            return config
+          },
         }
       ]
     }
